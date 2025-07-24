df70f2975f90b1190800c3618bba82caa7ad3d00
zakariaelaoufi <zakariaaoufi8@gmail.com>
2025-07-24 19:52:13 +0000

    deploy the model in the training notebook

--- inference.py
+++ inference.py
@@ -1,17 +1,13 @@
-import boto3
-import sagemaker
-from sagemaker import get_execution_role()
-import pandas as pd
-import numpy as np
 import joblib
 import json
 import os
+import numpy as np
 from io import StringIO
 
 
 def model_fn(model_dir):
     """Load the trained model for inference."""
-    return joblib.load(os.path.join(model_dir, "model.tar.gz"))
+    return joblib.load(os.path.join(model_dir, "model.joblib"))
 
 
 def input_fn(request_body, request_content_type):
@@ -19,7 +15,7 @@ def input_fn(request_body, request_content_type):
         data = json.loads(request_body)
         if isinstance(data, dict):
             data = [data[key] for key in data.keys()]
-        return np.array(data).reshape(1,-1)
+        return np.array(data).reshape(1, -1)
     elif request_content_type == 'text/csv':
         return np.loadtxt(StringIO(request_body), delimiter=",")
     else:
@@ -33,7 +29,6 @@ def output_fn(prediction, response_content_type):
         return ",".join(map(str, prediction))
     else:
         raise ValueError(f"Unsupported response content type: {response_content_type}")
-
 
 
 def predict_fn(input_data, model):
